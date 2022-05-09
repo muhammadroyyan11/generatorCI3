73aba9f1727e9be4d5541ae27a0c3f38a0924a02
muhammadroyyan11 <royyanmz87@gmail.com>
2022-05-09 15:35:58 +0700

    cok jancok wes dadi

---
+++ application/views/kategori/kategori_form.php
@@ -0,0 +1,32 @@
+<div class="content-wrapper">
+	<section class="content">
+		<div class="box box-warning box-solid">
+			<div class="box-header with-border">
+				<h3 class="box-title"><?php echo strtoupper($button) ?> DATA KATEGORI</h3>
+			</div>
+			<form action="<?php echo $action; ?>" method="post">
+			
+				<table class='table table-bordered'>
+	
+					<tr>
+						<td width='200'>Nama <?php echo form_error('nama') ?></td><td><input type="text" class="form-control" name="nama" id="nama" placeholder="Nama" value="<?php echo $nama; ?>" /></td>
+					</tr>
+	
+					<tr>
+						<td width='200'>IsActive <?php echo form_error('isActive') ?></td><td><input type="text" class="form-control" name="isActive" id="isActive" placeholder="IsActive" value="<?php echo $isActive; ?>" /></td>
+					</tr>
+	
+					<tr>
+						<td></td>
+						<td>
+							<input type="hidden" name="id_kategori" value="<?php echo $id_kategori; ?>" /> 
+							<button type="submit" class="btn btn-danger"><i class="fa fa-floppy-o"></i> <?php echo $button ?></button> 
+							<a href="<?php echo site_url('kategori') ?>" class="btn btn-info"><i class="fa fa-sign-out"></i> Kembali</a>
+						</td>
+					</tr>
+	
+				</table>
+			</form>
+		</div>
+	</section>
+</div>
